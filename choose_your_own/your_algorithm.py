4b83c5e9e7ad107801bbb5e5bc789a874251228b
randallpo <polawatpo>
2018-09-10 19:34:24 +0700

    Finish Choose you own

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -30,13 +30,27 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+clf = KNeighborsClassifier(n_neighbors=8)
+clf.fit(features_train, labels_train) 
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print "KNeighborsClassifier accuracy: ", acc
 
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(max_depth=2, random_state=0)
+clf.fit(features_train, labels_train) 
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print "RandomForestClassifier accuracy: ", acc
 
-
-
-
-
-
+from sklearn.ensemble import AdaBoostClassifier
+clf = AdaBoostClassifier(n_estimators=30,learning_rate=0.5)
+clf.fit(features_train, labels_train) 
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print "AdaBoostClassifier accuracy: ", acc
 
 try:
     prettyPicture(clf, features_test, labels_test)
