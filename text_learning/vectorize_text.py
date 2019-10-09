5b347b4e3b4d4448447464f53de95dd4bbe4af18
Randall Po <polawatpo@msn.com>
2019-10-09 21:11:08 +0700

    Finish text learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -3,9 +3,15 @@
 import os
 import pickle
 import re
-import sys
+import sys, platform
+if platform.system() == 'Windows':
+    path = "d:/projects/ud120-projects/tools/"
+if platform.system() == 'Linux':
+    path = "/home/randall/projects/ud120-projects/tools/"
+if platform.system() == 'Darwin':
+    path = "/Users/randallpo/projects/ud120-projects/tools/"
+sys.path.append(path)
 
-sys.path.append( "../tools/" )
 from parse_out_email_text import parseOutText
 
 """
@@ -23,8 +29,8 @@ from parse_out_email_text import parseOutText
 """
 
 
-from_sara  = open("from_sara.txt", "r")
-from_chris = open("from_chris.txt", "r")
+from_sara  = open("text_learning/from_sara.txt", "r")
+from_chris = open("text_learning/from_chris.txt", "r")
 
 from_data = []
 word_data = []
@@ -43,19 +49,24 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
         if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
+            #path = os.path.join('..', path[:-1])
+            path = os.path.join('', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            text = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            for str in ["sara", "shackleton", "chris", "germani"]:
+                text.replace(str, "")
             ### append the text to word_data
-
+            word_data.append(text)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == "sara":
+                from_data.append(0)
+            if name == "chris":
+                from_data.append(1)
 
             email.close()
 
@@ -63,13 +74,16 @@ print "emails processed"
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+pickle.dump( word_data, open("text_learning/your_word_data.pkl", "w") )
+pickle.dump( from_data, open("text_learning/your_email_authors.pkl", "w") )
 
-
+print word_data[152]
 
 
 
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-
+vectorizer = TfidfVectorizer(stop_words="english")
+X = vectorizer.fit_transform(word_data)
+print len(vectorizer.get_feature_names())
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,22 +28,24 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+        stemmer = SnowballStemmer("english")
 
-
-
-
+        data = text_string.split()
+        stem_word = []
+        for word in data:
+            stem_word.append(stemmer.stem(word))
+        words = " ".join(stem_word) 
     return words
 
     
 
 def main():
-    ff = open("../text_learning/test_email.txt", "r")
+    ff = open("text_learning/test_email.txt", "r")
     text = parseOutText(ff)
     print text
 
